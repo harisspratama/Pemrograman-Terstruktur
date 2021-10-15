50d5fa973f57b50bb34235f532a936e2041a617d
harisspratama <90229841+harisspratama@users.noreply.github.com>
2021-10-15 22:00:16 +0700

    Delete Chapter 5/Praktikum_1 directory

--- Chapter 5/Praktikum_1/latihan1.py
+++
@@ -1,9 +0,0 @@
-print("***Status Kelulusan Mahasiswa***")
-Nama= (input("Nama : "))
-MTK= int(input("Nilai Matematika : ")) 
-BHSINDO= int(input("Nilai Bahasa Indonesia :"))
-IPA= int(input("Nilai IPA :"))
-if(MTK>70) and (BHSINDO>60) and (IPA>60):
-    print("Status Kelulusan : ", Nama,"DINYATAKAN LULUS")
-else:
-    print("Status Kelulusan : ", Nama,"DINYATAKAN TIDAK LULUS")
