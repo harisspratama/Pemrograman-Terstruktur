9f396326294a94ec52b7a18d347548b158fd2ed2
harisspratama <90229841+harisspratama@users.noreply.github.com>
2021-10-16 22:29:18 +0700

    Chapter 5

---
+++ Chapter 5/Praktikum 1/latihan1.py
@@ -0,0 +1,9 @@
+print("***Status Kelulusan Mahasiswa***")
+Nama= (input("Nama : "))
+MTK= int(input("Nilai Matematika : ")) 
+BHSINDO= int(input("Nilai Bahasa Indonesia :"))
+IPA= int(input("Nilai IPA :"))
+if(MTK>70) and (BHSINDO>=60) and (IPA>=60):
+    print("Status Kelulusan : ", Nama,"DINYATAKAN LULUS")
+else:
+    print("Status Kelulusan : ", Nama,"DINYATAKAN TIDAK LULUS")
