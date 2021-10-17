fd0552f406beab6d3495fa88a2350cdc406fb119
harisspratama <90229841+harisspratama@users.noreply.github.com>
2021-10-18 00:39:18 +0700

    Update latihan2.py

--- Chapter 5/Praktikum 1/latihan2.py
+++ Chapter 5/Praktikum 1/latihan2.py
@@ -5,7 +5,7 @@ BHSINDO= int(input("Nilai Bahasa Indonesia :"))
 IPA= int(input("Nilai IPA :"))
 if(MTK<0) or (BHSINDO<0) or (IPA<0) or (MTK>100) or (BHSINDO>100) or (IPA>100):
     print("Maaf input ada yang tidak valid")
-elif(MTK>70) and (BHSINDO>60) and (IPA>60):
+elif(MTK>70) and (BHSINDO>=60) and (IPA>=60):
     print("Status Kelulusan : ", Nama,"DINYATAKAN LULUS")
 else:
     print("Status Kelulusan : ", Nama,"DINYATAKAN TIDAK LULUS")
